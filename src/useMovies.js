0db64ab5639f181a6c96cadcac376ed3d882b6eb
Arka Sheikh <skarka90@gmail.com>
2023-07-04 13:06:26 +0530

    Final Commit

---
+++ src/useMovies.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+export function useMovies(query, callback) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const KEY = "2fc3c102";
+  useEffect(() => {
+    callback?.();
+    const controller = new AbortController();
+    async function fetchMovies() {
+      try {
+        setIsLoading(true);
+        setError("");
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+
+        if (!res.ok)
+          throw new Error("Something went wrong with fetching movies");
+
+        const data = await res.json();
+        if (data.Response === "False") throw new Error("No movie found");
+        setMovies(data.Search);
+        setError("");
+      } catch (err) {
+        if (err.name !== "AbortError") setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    if (query.length < 3) {
+      setMovies([]);
+      setError("");
+      return;
+    }
+    fetchMovies();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
+
+  return { movies, isLoading, error };
+}
